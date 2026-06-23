7180ad1bdc03251bd8d1d01b466b0fc3fea8c1c6
tracer-nju <tracer@nju.edu.cn>
2026-06-23 22:38:05 +0800

    Linux shan 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  22:38:05 up  9:13,  2 users,  load average: 0.56, 0.78, 0.75

--- .shadow/pstree/pstree.c
+++ .shadow/pstree/pstree.c
@@ -308,7 +308,7 @@ int main(int argc, char *argv[]) {
             }
             parent->child_capacity = new_capacity;
         }
-        proc->children[proc->child_count++] = parent;
+        parent->children[proc->child_count++] = *proc;
     }
 
     printf("Collected %zu processes:\n", processes.count);
