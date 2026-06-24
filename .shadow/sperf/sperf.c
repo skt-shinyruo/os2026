d8b26d1936220e04759fc3b8c0df07c35505330c
tracer-nju <tracer@nju.edu.cn>
2026-06-24 15:43:36 +0800

    Linux shan 6.6.87.2-microsoft-standard-WSL2 #1 SMP PREEMPT_DYNAMIC Thu Jun  5 18:30:46 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  15:43:36 up 18:23,  2 users,  load average: 0.37, 0.38, 0.39

--- .shadow/sperf/sperf.c
+++ .shadow/sperf/sperf.c
@@ -43,7 +43,7 @@ int main(int argc, char *argv[]) {
         printf("argv[%d]: %s\n", i, argv[i]);
         exec_argv[i + 1] = argv[i];
     }
-    execve("strace", exec_argv, exec_envp);
+    execve("strace", argv, exec_envp);
     perror(argv[0]);
     exit(EXIT_FAILURE);
 }
